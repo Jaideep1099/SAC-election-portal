a965ec131878fc65a139058d6951c231a33a6359
DragonPyli <codewizard001@gmail.com>
2021-10-20 15:36:19 +0530

    Major Security Updates         - Added user checks to backend routes         - Added Admin checks to backend routes         - Major Frontend requests now sent with tokens

--- Backend/auth.py
+++ Backend/auth.py
@@ -2,6 +2,6 @@ def authenticate_user(cursor, uname, token):
     
     cursor.execute("SELECT COUNT(*) FROM SESSION WHERE ROLLNO=%s AND TOKEN=%s", (uname, token))
     res = cursor.fetchone()
-    # p_rint(res)
+    # p_rint(res) 
     
     return res[0]==1
